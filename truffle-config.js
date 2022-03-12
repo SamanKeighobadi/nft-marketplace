1bd75db262fae936d83bd5590e9419f9e6a9c0a1
SamanKeighobadi <saman.keighobadi@gmail.com>
2022-03-12 20:00:30 +0330

    Initial preparation
    set bootstrap and storage location of abis

--- truffle-config.js
+++ truffle-config.js
@@ -3,9 +3,9 @@ const path = require("path");
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  contracts_build_directory: path.join(__dirname, "client/src/abis"),
   networks: {
-    develop: {
+    development: {
       port: 8545
     }
   }
