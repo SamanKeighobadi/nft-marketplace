44a34cda2f3234674f30cd23dc57d48a35f96a5e
SamanKeighobadi <saman.keighobadi@gmail.com>
2022-03-12 21:33:13 +0330

    fix: replace dependencies
    dependencies are incorectly install out of client folder

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import {render} from 'react-dom';
-import './index.css';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.css'
+import React from "react";
+import { render } from "react-dom";
+import "./index.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter } from "react-router-dom";
 
-render(<App />, document.getElementById('root'));
-
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
