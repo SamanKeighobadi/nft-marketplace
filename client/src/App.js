e270370875c4b88c22e42f08b01eb397bf8d4e3d
SamanKeighobadi <saman.keighobadi@gmail.com>
2022-03-12 21:41:14 +0330

    create navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React  from "react";
+import AppNavbar from "./components/AppNavbar";
 // React Router DOM
 
 const App = () =>{
   return (
     <div>
+    <AppNavbar />
       <h1>NFT marketeplace</h1>
     </div>
   )
