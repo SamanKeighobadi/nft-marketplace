266283b96594a63406ecbbb7dac25b0580c2c669
SamanKeighobadi <saman.keighobadi@gmail.com>
2022-03-12 22:14:02 +0330

    create pages and routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,26 @@
-import React  from "react";
+import React from "react";
+// MainLayout
 import MainLayout from "./components/layout/MainLayout";
+// React Router DOM
+import { Routes, Route } from "react-router-dom";
+// Custom Components
+import Home from "./pages/Home";
+import CreateNFT from "./pages/CreateNFT";
+import Purchases from "./pages/Purchases/Purchases";
+import Lists from "./pages/nft-lists/Lists";
 
-
-const App = () =>{
+const App = () => {
   return (
     <MainLayout>
-      <h1>NFT marketeplace</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<CreateNFT />} />
+        <Route path="/purchases" element={<Purchases />} />
+        <Route path="/items-list" element={<Lists />} />
+        
+      </Routes>
     </MainLayout>
-  )
-}
+  );
+};
 
 export default App;
---
+++ client/src/pages/CreateNFT.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const CreateNFT = () => {
+    return ( 
+        <div>
+            <h1>Create NFT</h1>
+        </div>
+     );
+}
+ 
+export default CreateNFT;
