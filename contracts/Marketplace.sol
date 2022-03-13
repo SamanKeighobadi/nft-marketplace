7b06e89d1e424ca92fd5b1daee72ca3eb5bc8e06
SamanKeighobadi <saman.keighobadi@gmail.com>
2022-03-13 19:39:44 +0330

    feat: marketplace contract
    create defined variables and create migration  file

---
+++ contracts/Marketplace.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract Marketplace is ReentrancyGuard {
+    // varibalse
+    address payable public immutable feeAccount; // the account that receives fees
+    uint256 public immutable feePercent; // the fee percentage on sales
+    uint256 public itemCount;
+
+    constructor(uint256 _feePercent) {
+        feeAccount = payable(msg.sender);
+        feePercent = _feePercent;
+    }
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var NFT = artifacts.require("./NFT.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+  deployer.deploy(NFT);
 };
