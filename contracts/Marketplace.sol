8f4e73d1decec4a1c0bb234a6e45387c83c12960
SamanKeighobadi <saman.keighobadi@gmail.com>
2022-03-13 20:37:30 +0330

    feat:   create nft item
    make nft and put them inside of mapping

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -10,8 +10,36 @@ contract Marketplace is ReentrancyGuard {
     uint256 public immutable feePercent; // the fee percentage on sales
     uint256 public itemCount;
 
+    struct Item {
+        uint itemId;
+        IERC721 nft;
+        uint tokenId;
+        uint price;
+        address payable seller;
+        bool sold;
+    }
+
+    mapping(uint => Item) public items;
+
     constructor(uint256 _feePercent) {
         feeAccount = payable(msg.sender);
         feePercent = _feePercent;
     }
+
+    function makeItem(IERC721 _nft,uint _tokenId,uint _price) external nonReentrant {
+        require(_price>0,"must be greater than zero" );
+        itemCount ++;
+
+        _nft.transferFrom(msg.sender, address(this), _tokenId);
+
+        items[itemCount] = Item(
+            itemCount,
+            _nft,
+            _tokenId,
+            _price,
+            payable(msg.sender),
+            false
+        );
+    }
+
 }
