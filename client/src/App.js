e3e28f76dd2a2e501869cbe57b98125607328786
SamanKeighobadi <saman.keighobadi@gmail.com>
2022-03-16 12:46:56 +0330

    load and connect web3 and load blockchain network

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 // MainLayout
 import MainLayout from "./components/layout/MainLayout";
 // React Router DOM
@@ -9,16 +9,60 @@ import CreateNFT from "./pages/CreateNFT";
 import Purchases from "./pages/Purchases/Purchases";
 import Lists from "./pages/nft-lists/Lists";
 
+import Web3 from "web3";
+import MarketPlace from "./abis/MarketPlace.json";
+
 const App = () => {
+  const [account, setAccount] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  const loadWeb3 = async () => {
+    try {
+      if (window.ethereum) {
+        window.web3 = new Web3(window.ethereum);
+        await window.ethereum.enable();
+      } else if (window.web3) {
+        window.web3 = new Web3(window.web3.currentProvider);
+      } else {
+        setLoading(true);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const loadBlockchain = async () => {
+    try {
+      const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+      const accounts = await web3.eth.getAccounts();
+      setAccount(accounts[0]);
+
+      const networkId = await web3.eth.net.getId();
+      const networkData = await MarketPlace.networks[networkId];
+
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    loadWeb3();
+    loadBlockchain();
+  }, []);
+
   return (
     <MainLayout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create" element={<CreateNFT />} />
-        <Route path="/purchases" element={<Purchases />} />
-        <Route path="/items-list" element={<Lists />} />
-        
-      </Routes>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<CreateNFT />} />
+          <Route path="/purchases" element={<Purchases />} />
+          <Route path="/items-list" element={<Lists />} />
+        </Routes>
+      )}
     </MainLayout>
   );
 };
--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -2,20 +2,17 @@ import React from "react";
 // React Router DOM
 import { Link } from "react-router-dom";
 
-
 const AppNavbar = () => {
   return (
     <div>
       <nav
-        class="navbar navbar-expand-lg navbar-dark bg-dark"
+        className="navbar navbar-expand-lg navbar-dark bg-dark"
         aria-label="Fifth navbar example"
       >
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-
-          </a>
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#"></a>
           <button
-            class="navbar-toggler"
+            className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarsExample05"
@@ -23,27 +20,30 @@ const AppNavbar = () => {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
 
-          <div class="collapse navbar-collapse" id="navbarsExample05">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <Link class="nav-link " to={"/"}>
+          <div className="collapse navbar-collapse" id="navbarsExample05">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link " to={"/"}>
                   Home
                 </Link>
               </li>
-              <li class="nav-item">
-                <Link className="nav-link" to={"/create"}>Create</Link>
-              </li>
-              <li class="nav-item">
-                
-                <Link class="nav-link " to={"/purchases"}>Purchases</Link>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/create"}>
+                  Create
+                </Link>
               </li>
               <li className="nav-item">
-                  <Link className="nav-link" to={'/items-list'}>
-                      Lists
-                  </Link>
+                <Link className="nav-link " to={"/purchases"}>
+                  Purchases
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/items-list"}>
+                  Lists
+                </Link>
               </li>
             </ul>
           </div>
